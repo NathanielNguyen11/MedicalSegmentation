0d12e7482b4e9a21655eb1b318a497ff4ecaef64
NathanielNguyen11 <32150689+NathanielNguyen11@users.noreply.github.com>
2019-06-11 16:47:05 +0900

    Add files via upload

---
+++ dataset/testimage.py
@@ -0,0 +1,40 @@
+#@title Imports
+
+import os
+from io import BytesIO
+import tarfile
+import tempfile
+from six.moves import urllib
+
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+from PIL import Image
+
+import tensorflow as tf
+
+SAMPLE_IMAGE = 'image1'  # @param ['image1', 'image2', 'image3']
+IMAGE_URL = '/home/ubuntu/Desktop/1.png'  #@param {type:"string"}
+
+_SAMPLE_URL = ('https://github.com/tensorflow/models/blob/master/research/'
+               'deeplab/g3doc/img/%s.jpg?raw=true')
+
+
+def run_visualization(url):
+  """Inferences DeepLab model and visualizes result."""
+  try:
+    f = urllib.request.urlopen(url)
+    jpeg_str = f.read()
+    original_im = Image.open(BytesIO(jpeg_str))
+  except IOError:
+    print('Cannot retrieve image. Please check url: ' + url)
+    return
+
+  print('running deeplab on image %s...' % url)
+  resized_im, seg_map = MODEL.run(original_im)
+
+  vis_segmentation(resized_im, seg_map)
+
+if __name__ == '__main__':
+
+  run_visualization(image_url)
